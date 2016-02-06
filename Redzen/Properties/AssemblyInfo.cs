5f30dfb0b9301e620d6c75c1a4058aafbf83cb53
colgreen <colin.green1@gmail.com>
2016-02-06 15:49:15 +0000

    csproj is set to sign the built assembly, but using a certificate (pfx file) that is excluded from the project (csproj). Updated the nuspec to use substitution variables for assembly version, author etc.

--- Redzen/Properties/AssemblyInfo.cs
+++ Redzen/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Redzen")]
 [assembly: AssemblyDescription("General purpose C# code library.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Colin D. Green")]
 [assembly: AssemblyProduct("Redzen")]
 [assembly: AssemblyCopyright("Copyright Colin D. Green 2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
