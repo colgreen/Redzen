eccbc250b5af50bab5cf6c5c99fd3e02d9b18be5
Colin Green <colin.green1@gmail.com>
2017-04-10 21:53:32 +0100

    Added SamplingWithoutReplacement.

---
+++ Redzen.UnitTests/SamplingWithoutReplacementTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Redzen.Numerics;
+
+namespace Redzen.UnitTests
+{
+    [TestClass]
+    public class SamplingWithoutReplacementTests
+    {
+        [TestMethod]
+        [TestCategory("SamplingWithoutReplacement")]
+        public void SampleAllChoices()
+        {
+            const int size = 5;
+            XorShiftRandom rng = new XorShiftRandom();
+
+            // Sample all of the elements.
+            int[] sampleArr = SamplingWithoutReplacement.TakeSamples(size, size, rng);
+
+            // Sort the samples.
+            Array.Sort(sampleArr);
+
+            // Confirm that all of the choices were selected.
+            for(int i=0; i<size; i++) {
+                Assert.AreEqual(i, sampleArr[i]);
+            }
+        }
+    }
+}
---
+++ Redzen/Numerics/SamplingWithoutReplacement.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Redzen.Numerics
+{
+    public static class SamplingWithoutReplacement
+    {
+        #region Public Static Methods
+
+        public static int[] TakeSamples(int numberOfChoices, int sampleCount, IRandomSource rng)
+        {
+            if(sampleCount > numberOfChoices) {
+                throw new ArgumentException("sampleCount must be less then or equal to numberOfChoices.");
+            }
+
+            // Create an array of indexes, one index per possible choice.
+            int[] indexArr = new int[numberOfChoices];
+            for(int i=0; i<numberOfChoices; i++) {
+                indexArr[i] = i;
+            }
+
+            // Sample loop.
+            for(int i=0; i<sampleCount; i++)
+            {
+                // Select an index at random.
+                int idx = rng.Next(i, numberOfChoices);
+
+                // Swap elements i and idx.
+                Swap(indexArr, i, idx);
+            }
+
+            // Copy the samples into an array.
+            int[] samplesArr = new int[sampleCount];
+            for(int i=0; i<sampleCount; i++) {
+                samplesArr[i] = indexArr[i];
+            }
+
+            return samplesArr;
+        }
+
+        #endregion
+
+        #region Private Static Methods
+
+        private static void Swap(int[] arr, int x, int y)
+        {
+            int tmp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = tmp;
+        }
+
+        #endregion
+    }
+}
