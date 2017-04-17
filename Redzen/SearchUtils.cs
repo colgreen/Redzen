d289ce38fb65049a62a428173702667ebbf2070a
Colin Green <colin.green1@gmail.com>
2017-04-17 18:50:26 +0100

    Binary search on a sub-range

--- Redzen/SearchUtils.cs
+++ Redzen/SearchUtils.cs
@@ -20,11 +20,28 @@ namespace Redzen
         /// element that is larger than item or, if there is no larger element, the bitwise complement of list.Count.</returns>
         public static int BinarySearch<T,V>(IList<T> list, V value, Func<T,V,int> compareFn)
         {
-			int lower = 0;
-			int upper = list.Count - 1;
-			while (lower <= upper)
+            return BinarySearch(list, 0, list.Count, value, compareFn);
+        }
+
+        /// <summary>
+        /// Searches a sub-range of a one-dimensional sorted list for a specific item, using the provided comparison function.
+        /// </summary>
+        /// <typeparam name="T">The list item type.</typeparam>
+        /// <typeparam name="V">The type of value being searched on.</typeparam>
+        /// <param name="list">The list of items to search.</param>
+        /// <param name="index">The starting index of the range to search.</param>
+        /// <param name="length">The length of the range to search.</param>
+        /// <param name="value">The value being search for.</param>
+        /// <param name="compareFn">Comparison function. For comparing list items based on the return value of this function rather than the items themselves.</param>
+        /// <returns>The zero-based index of item in the list, if item is found; otherwise, a negative number that is the bitwise complement of the index of the next 
+        /// element that is larger than item or, if there is no larger element, the bitwise complement of list.Count.</returns>
+        public static int BinarySearch<T,V>(IList<T> list, int index, int length, V value, Func<T,V,int> compareFn)
+        {
+			int lo = index;
+			int hi = index + length - 1;
+			while (lo <= hi)
 			{
-				int mid = lower + (upper - lower >> 1);
+				int mid = lo + (hi - lo >> 1);
 
 				int cmp = compareFn(list[mid], value);
 				if (cmp == 0) {
@@ -32,12 +49,12 @@ namespace Redzen
 				}
 
 				if (cmp < 0) {
-					lower = mid + 1;
+					lo = mid + 1;
 				} else {
-					upper = mid - 1;
+					hi = mid - 1;
 				}
 			}
-			return ~lower;
+			return ~lo;
         }
     }
 }
