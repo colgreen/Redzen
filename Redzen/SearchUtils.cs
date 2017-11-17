717aea69a12b44a2faa42e0c507913fefef27ea3
Colin Green <colin.green1@gmail.com>
2017-11-17 14:37:07 +0000

    Comment tweaks.

--- Redzen/SearchUtils.cs
+++ Redzen/SearchUtils.cs
@@ -16,7 +16,7 @@ namespace Redzen
         /// <param name="list">The list of items to search.</param>
         /// <param name="value">The value being search for.</param>
         /// <param name="compareFn">Comparison function. For comparing list items based on the return value of this function rather than the items themselves.</param>
-        /// <returns>The zero-based index of item in the list, if item is found; otherwise, a negative number that is the bitwise complement of the index of the next 
+        /// <returns>The zero-based index of an item in the list, if item is found; otherwise, a negative number that is the bitwise complement of the index of the next 
         /// element that is larger than item or, if there is no larger element, the bitwise complement of list.Count.</returns>
         public static int BinarySearch<T,V>(IList<T> list, V value, Func<T,V,int> compareFn)
         {
@@ -33,8 +33,9 @@ namespace Redzen
         /// <param name="length">The length of the range to search.</param>
         /// <param name="value">The value being search for.</param>
         /// <param name="compareFn">Comparison function. For comparing list items based on the return value of this function rather than the items themselves.</param>
-        /// <returns>The zero-based index of item in the list, if item is found; otherwise, a negative number that is the bitwise complement of the index of the next 
-        /// element that is larger than item or, if there is no larger element, the bitwise complement of list.Count.</returns>
+        /// <returns>The zero-based index of an item in the list, if item is found; otherwise, a negative number that is 
+        /// the bitwise complement of the index of the next element that is larger than item or, if there is no larger element,
+        /// the bitwise complement of list.Count.</returns>
         public static int BinarySearch<T,V>(IList<T> list, int index, int length, V value, Func<T,V,int> compareFn)
         {
 			int lo = index;
