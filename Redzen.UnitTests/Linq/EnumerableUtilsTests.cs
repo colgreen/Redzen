90e836852d5b6b5f69f918c01802266d456ea824
Colin Green <colin.green1@gmail.com>
2017-12-29 22:28:55 +0000

    Added Linq.EnumerableUtils + unit test.

---
+++ Redzen.UnitTests/Linq/EnumerableUtilsTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Redzen.Linq;
+using Redzen.Random;
+
+namespace Redzen.UnitTests.Linq
+{
+    [TestClass]
+    public class EnumerableUtilsTests
+    {
+        [TestMethod]
+        [TestCategory("EnumerableUtils")]
+        public void TestRangeRandomOrder()
+        {
+            var rng = new XorShiftRandom(0);
+
+            RunTest(0, 100, rng);
+            RunTest(20, 100, rng);
+        }
+
+        private static void RunTest(int start, int count, IRandomSource rng)
+        {
+            // Enumerate the sequence and store the result in an array.
+            int[] arr = EnumerableUtils.RangeRandomOrder(start, count, rng).ToArray();
+
+            // Simple tests.
+            Assert.AreEqual(count, arr.Length);
+            Assert.AreEqual(start, arr.Min());
+            Assert.AreEqual(start+count-1, arr.Max());
+
+            // Test for dupes.
+            Assert.AreEqual(0, arr.GroupBy(x => x)
+                                .Where(g => g.Count() > 1)
+                                .Select(y => y.Key).Count());
+                            
+            // Basic randomness test.
+            // As we progress through the sequence, count the number of times and entry is higher and lower than the previous entry.
+            // For an ordered sequence the 'lower' count will be zero. For pure random the two counts have an expected value of 50/50, but
+            // in any given sequence hey may not be anywhere near that. By fixing the random seed (above) we at least ensure that the unit test 
+            // will pass rather than mostly passing!
+            int highCount = 0, lowCount = 0;
+            
+            for(int i=1; i < arr.Length; i++)
+            {
+                if(arr[i] > arr[i-1]) {
+                    highCount++;
+                }
+                else {
+                    lowCount++;    
+                }
+            }
+
+            Assert.IsTrue(highCount > 30);
+            Assert.IsTrue(lowCount > 30);
+        }
+    }
+}
---
+++ Redzen/Linq/EnumerableUtils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Redzen.Random;
+
+namespace Redzen.Linq
+{
+    /// <summary>
+    /// Utility methods related to LINQ and IEnumerable.
+    /// </summary>
+    public class EnumerableUtils
+    {
+        /// <summary>Generates a sequence of integral numbers within a specified range and in random order.</summary>
+        /// <param name="start">The value of the first integer in the sequence.</param>
+        /// <param name="count">The number of sequential integers to generate.</param>
+        /// <returns>A new IEnumerable{int}.</returns>
+        public static IEnumerable<int> RangeRandomOrder(int start, int count, IRandomSource rng)
+        {
+	        long numl = (long)start + (long)count - 1L;
+	        if (count < 0 || numl > 2147483647L) {
+		        throw new ArgumentOutOfRangeException("count");
+	        }
+
+            // Create an array of all indexes to be yielded.
+            int[] arr = new int[count];
+            for(int i=0; i < count; i++) {
+                arr[i] = start + i;
+            }
+
+            // Yield all values in turn, applying a Fisher–Yates shuffle as we go in order to randomize the yield order.
+            // See https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+	        for (int i=count-1; i > 0; i--)
+	        {
+                int swapIdx = rng.Next(i + 1);
+                int tmp = arr[swapIdx];
+                arr[swapIdx] = arr[i];
+                arr[i] = tmp;
+
+		        yield return arr[i];
+	        }
+
+            // Yield final value.
+            yield return arr[0];	        
+        }
+    }
+}
