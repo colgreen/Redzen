df6ddc1220fe4b25a423a9f6f7ca064a93157a2c
Colin Green <colin.green1@gmail.com>
2018-02-12 20:43:16 +0000

    Added VariableUtils.Swap()

---
+++ Redzen/VariableUtils.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Redzen
+{
+    /// <summary>
+    /// General purpose helper methods.
+    /// </summary>
+    public static class VariableUtils
+    {
+        /// <summary>
+        /// Swap two variables.
+        /// </summary>
+        /// <typeparam name="T">Variable type.</typeparam>
+        /// <param name="a">First variable.</param>
+        /// <param name="b">Second variable.</param>
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            var tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+}
