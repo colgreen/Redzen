43f34c4c7bb39b33b99a9158995da92621ed5e6d
Colin Green <colin.green1@gmail.com>
2018-05-05 21:58:30 +0100

    Added XoroShiro128PlusRandomBenchmark.

---
+++ Redzen.Benchmarks/XoroShiro128PlusRandomBenchmark.cs
@@ -0,0 +1,108 @@
+﻿using BenchmarkDotNet.Attributes;
+using Redzen.Random;
+
+namespace Redzen.Benchmarks
+{
+    public class XoroShiro128PlusRandomBenchmark
+    {
+        const int __loops = 10_000_000;
+        XoroShiro128PlusRandom _rng = new XoroShiro128PlusRandom();
+        byte[] _buff = new byte[1_000_000];
+
+        #region Benchmark Methods [System.Random Equivalents]
+
+        [Benchmark]
+        public void Next10M()
+        {
+            for(int i=0; i<__loops; i++) {
+                _rng.Next();
+            }
+        }
+
+        [Benchmark]
+        public void NextUpperBound10M()
+        {
+            for(int i=0; i<__loops; i++) {
+                _rng.Next(123);
+            }
+        }
+
+        [Benchmark]
+        public void NextLowerUpperBound10M()
+        {
+            for(int i=0; i<__loops; i++) {
+                _rng.Next(1000, 10_000);
+            }
+        }
+
+        [Benchmark]
+        public void NextDouble10M()
+        {
+            for(int i=0; i<__loops; i++) {
+                _rng.NextDouble();
+            }
+        }
+
+        [Benchmark]
+        public void NextBytes100M()
+        {
+            for(int i=0; i<100; i++) {
+                _rng.NextBytes(_buff);
+            }
+        }
+
+        #endregion
+
+        #region Benchmark Methods [Methods not present on System.Random]
+
+        [Benchmark]
+        public void NextFloat10M()
+        {
+            for(int i=0; i<__loops; i++) {
+                _rng.NextFloat();
+            }
+        }
+
+        [Benchmark]
+        public void NextUInt10M()
+        {
+            for(int i=0; i<__loops; i++) {
+                _rng.NextUInt();
+            }
+        }
+
+        [Benchmark]
+        public void NextInt()
+        {
+            for(int i=0; i<__loops; i++) {
+                _rng.NextInt();
+            }
+        }
+
+        [Benchmark]
+        public void NextDoubleNonZero10M()
+        {
+            for(int i=0; i<__loops; i++) {
+                _rng.NextDoubleNonZero();
+            }
+        }
+
+        [Benchmark]
+        public void NextBool()
+        {
+            for(int i=0; i<__loops; i++) {
+                _rng.NextBool();
+            }
+        }
+
+        [Benchmark]
+        public void NextByte()
+        {
+            for(int i=0; i<__loops; i++) {
+                _rng.NextByte();
+            }
+        }
+
+        #endregion
+    }
+}
