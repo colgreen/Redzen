89465e5594c63b1819d6e7597cede7362fdf14e6
Colin Green <colin.green1@gmail.com>
2018-05-08 16:36:55 +0100

    Added MathUtils.CeilingToPowerOfTwo(long), IsPowerOfTwo(long), Log2(int and long).

--- Redzen.UnitTests/MathUtilsTests.cs
+++ Redzen.UnitTests/MathUtilsTests.cs
@@ -7,19 +7,126 @@ namespace Redzen.UnitTests
     {
         [TestMethod]
         [TestCategory("MathUtils")]
-        public void CeilingPower2()
+        public void CeilingToPowerOfTwo_Int32()
         {
-            for(int i=0; i < 100000000; i++)
-            {
-                int result = MathUtils.CeilingPowerOfTwo(i);
+            // Note. Zero isn't really a power of two!
+            Assert.AreEqual(0, MathUtils.CeilingToPowerOfTwo(0));
+            Assert.AreEqual(1, MathUtils.CeilingToPowerOfTwo(1));
+            Assert.AreEqual(2, MathUtils.CeilingToPowerOfTwo(2));
+            Assert.AreEqual(4, MathUtils.CeilingToPowerOfTwo(3));
+            Assert.AreEqual(4, MathUtils.CeilingToPowerOfTwo(4));
+            Assert.AreEqual(8, MathUtils.CeilingToPowerOfTwo(5));
+            Assert.AreEqual(8, MathUtils.CeilingToPowerOfTwo(7));
+            Assert.AreEqual(8, MathUtils.CeilingToPowerOfTwo(8));
 
-                Assert.IsTrue(result >= i);
-                Assert.IsTrue(i == 0 || MathUtils.IsPowerOfTwo(result));
+            Assert.AreEqual(16, MathUtils.CeilingToPowerOfTwo(9));
+            Assert.AreEqual(16, MathUtils.CeilingToPowerOfTwo(15));
+            Assert.AreEqual(16, MathUtils.CeilingToPowerOfTwo(16));
 
-                // Note. This could be zero, which is generally not considered to be a power of two, but is suitable for this test.
-                int prevPowerOfTwo = result >> 1;
-                Assert.IsTrue(i ==0 || prevPowerOfTwo < i);
-            }
+            Assert.AreEqual(32, MathUtils.CeilingToPowerOfTwo(17));
+            Assert.AreEqual(32, MathUtils.CeilingToPowerOfTwo(31));
+            Assert.AreEqual(32, MathUtils.CeilingToPowerOfTwo(32));
+
+            Assert.AreEqual(0x2000_0000, MathUtils.CeilingToPowerOfTwo(0x1000_0001));
+            Assert.AreEqual(0x2000_0000, MathUtils.CeilingToPowerOfTwo(0x1FFF_FFFF));
+            Assert.AreEqual(0x2000_0000, MathUtils.CeilingToPowerOfTwo(0x2000_0000));
+
+            Assert.AreEqual(0x4000_0000, MathUtils.CeilingToPowerOfTwo(0x2000_0001));
+            Assert.AreEqual(0x4000_0000, MathUtils.CeilingToPowerOfTwo(0x3FFF_FFFF));
+            Assert.AreEqual(0x4000_0000, MathUtils.CeilingToPowerOfTwo(0x4000_0000));
+        }
+
+        [TestMethod]
+        [TestCategory("MathUtils")]
+        public void CeilingToPowerOfTwo_Int64()
+        {
+            // Note. Zero isn't really a power of two!
+            Assert.AreEqual(0, MathUtils.CeilingToPowerOfTwo(0L));
+            Assert.AreEqual(1, MathUtils.CeilingToPowerOfTwo(1L));
+            Assert.AreEqual(2, MathUtils.CeilingToPowerOfTwo(2L));
+            Assert.AreEqual(4, MathUtils.CeilingToPowerOfTwo(3L));
+            Assert.AreEqual(4, MathUtils.CeilingToPowerOfTwo(4L));
+            Assert.AreEqual(8, MathUtils.CeilingToPowerOfTwo(5L));
+            Assert.AreEqual(8, MathUtils.CeilingToPowerOfTwo(7L));
+            Assert.AreEqual(8, MathUtils.CeilingToPowerOfTwo(8L));
+
+            Assert.AreEqual(16, MathUtils.CeilingToPowerOfTwo(9L));
+            Assert.AreEqual(16, MathUtils.CeilingToPowerOfTwo(15L));
+            Assert.AreEqual(16, MathUtils.CeilingToPowerOfTwo(16L));
+
+            Assert.AreEqual(32, MathUtils.CeilingToPowerOfTwo(17L));
+            Assert.AreEqual(32, MathUtils.CeilingToPowerOfTwo(31L));
+            Assert.AreEqual(32, MathUtils.CeilingToPowerOfTwo(32L));
+
+            Assert.AreEqual(0x2000_0000, MathUtils.CeilingToPowerOfTwo(0x1000_0001L));
+            Assert.AreEqual(0x2000_0000, MathUtils.CeilingToPowerOfTwo(0x1FFF_FFFFL));
+            Assert.AreEqual(0x2000_0000, MathUtils.CeilingToPowerOfTwo(0x2000_0000L));
+
+            Assert.AreEqual(0x4000_0000, MathUtils.CeilingToPowerOfTwo(0x2000_0001L));
+            Assert.AreEqual(0x4000_0000, MathUtils.CeilingToPowerOfTwo(0x3FFF_FFFFL));
+            Assert.AreEqual(0x4000_0000, MathUtils.CeilingToPowerOfTwo(0x4000_0000L));
+
+            Assert.AreEqual(0x2000_0000_0000_0000, MathUtils.CeilingToPowerOfTwo(0x1000_0000_0000_0001L));
+            Assert.AreEqual(0x2000_0000_0000_0000, MathUtils.CeilingToPowerOfTwo(0x1FFF_FFFF_FFFF_FFFFL));
+            Assert.AreEqual(0x2000_0000_0000_0000, MathUtils.CeilingToPowerOfTwo(0x2000_0000_0000_0000L));
+
+            Assert.AreEqual(0x4000_0000_0000_0000, MathUtils.CeilingToPowerOfTwo(0x2000_0000_0000_0001L));
+            Assert.AreEqual(0x4000_0000_0000_0000, MathUtils.CeilingToPowerOfTwo(0x3FFF_FFFF_FFFF_FFFFL));
+            Assert.AreEqual(0x4000_0000_0000_0000, MathUtils.CeilingToPowerOfTwo(0x4000_0000_0000_0000L));
+        }
+
+        [TestMethod]
+        [TestCategory("MathUtils")]
+        public void Log2_Int32()
+        {
+            Assert.AreEqual(0, MathUtils.Log2(0x1));
+            Assert.AreEqual(1, MathUtils.Log2(0x2));
+            Assert.AreEqual(1, MathUtils.Log2(0x3));
+            Assert.AreEqual(2, MathUtils.Log2(0x4));
+            Assert.AreEqual(2, MathUtils.Log2(0x5));
+            Assert.AreEqual(2, MathUtils.Log2(0x6));
+            Assert.AreEqual(2, MathUtils.Log2(0x7));
+            Assert.AreEqual(3, MathUtils.Log2(0x8));
+            Assert.AreEqual(3, MathUtils.Log2(0x9));
+
+            Assert.AreEqual(3, MathUtils.Log2(0xF));
+            Assert.AreEqual(4, MathUtils.Log2(0x10));
+            Assert.AreEqual(4, MathUtils.Log2(0x11));
+
+            Assert.AreEqual(4, MathUtils.Log2(0x1F));
+            Assert.AreEqual(5, MathUtils.Log2(0x20));
+            Assert.AreEqual(5, MathUtils.Log2(0x21));
+
+            Assert.AreEqual(29, MathUtils.Log2(0x3FFF_FFFF));
+            Assert.AreEqual(30, MathUtils.Log2(0x4000_0000));
+            Assert.AreEqual(30, MathUtils.Log2(0x4000_0001));
+        }
+
+        [TestMethod]
+        [TestCategory("MathUtils")]
+        public void Log2_Int64()
+        {
+            Assert.AreEqual(0, MathUtils.Log2(0x1L));
+            Assert.AreEqual(1, MathUtils.Log2(0x2L));
+            Assert.AreEqual(1, MathUtils.Log2(0x3L));
+            Assert.AreEqual(2, MathUtils.Log2(0x4L));
+            Assert.AreEqual(2, MathUtils.Log2(0x5L));
+            Assert.AreEqual(2, MathUtils.Log2(0x6L));
+            Assert.AreEqual(2, MathUtils.Log2(0x7L));
+            Assert.AreEqual(3, MathUtils.Log2(0x8L));
+            Assert.AreEqual(3, MathUtils.Log2(0x9L));
+
+            Assert.AreEqual(3, MathUtils.Log2(0xFL));
+            Assert.AreEqual(4, MathUtils.Log2(0x10L));
+            Assert.AreEqual(4, MathUtils.Log2(0x11L));
+
+            Assert.AreEqual(4, MathUtils.Log2(0x1FL));
+            Assert.AreEqual(5, MathUtils.Log2(0x20L));
+            Assert.AreEqual(5, MathUtils.Log2(0x21L));
+
+            Assert.AreEqual(29, MathUtils.Log2(0x3FFF_FFFFL));
+            Assert.AreEqual(30, MathUtils.Log2(0x4000_0000L));
+            Assert.AreEqual(30, MathUtils.Log2(0x4000_0001L));
         }
     }
 }
--- Redzen/MathUtils.cs
+++ Redzen/MathUtils.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace Redzen
 {
     public static class MathUtils
@@ -8,20 +9,44 @@ namespace Redzen
         /// </summary>
         /// <param name="x"></param>
         /// <returns>The smallest integral power of two that is greater than or equal to x.</returns>
-        public static int CeilingPowerOfTwo(int x)
+        public static int CeilingToPowerOfTwo(int x)
         {
+            // Text for max input value. There is one more high bit, but that is the sign bit.
+            if (x < 0 || x > 0x4000_0000) {
+                throw new ArgumentOutOfRangeException(nameof(x));
+            }
+
             // From: https://stackoverflow.com/questions/466204/rounding-up-to-next-power-of-2
-            uint v = unchecked((uint)x); // compute the next highest power of 2 of 32-bit v
+            x--;
+            x |= x >> 1;
+            x |= x >> 2;
+            x |= x >> 4;
+            x |= x >> 8;
+            x |= x >> 16;
+            return x + 1;
+        }
 
-            v--;
-            v |= v >> 1;
-            v |= v >> 2;
-            v |= v >> 4;
-            v |= v >> 8;
-            v |= v >> 16;
-            v++;
+        /// <summary>
+        /// Returns the smallest power of two that is greater than or equal to the specified number.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns>The smallest integral power of two that is greater than or equal to x.</returns>
+        public static long CeilingToPowerOfTwo(long x)
+        {
+            // Text for max input value. There is one more high bit, but that is the sign bit.
+            if (x < 0 || x > 0x4000_0000_0000_0000){
+                throw new ArgumentOutOfRangeException(nameof(x));
+            }
 
-            return (int)v;
+            // From: https://stackoverflow.com/questions/466204/rounding-up-to-next-power-of-2
+            x--;
+            x |= x >> 1;
+            x |= x >> 2;
+            x |= x >> 4;
+            x |= x >> 8;
+            x |= x >> 16;
+            x |= x >> 32;
+            return x + 1;
         }
 
         /// <summary>
@@ -34,5 +59,72 @@ namespace Redzen
             // From: https://stackoverflow.com/questions/600293/how-to-check-if-a-number-is-a-power-of-2
             return (x != 0) && (x & (x - 1)) == 0;
         }
+
+        /// <summary>
+        /// Returns true if the input value is an integer power of two.
+        /// </summary>
+        /// <param name="x">The value to test.</param>
+        /// <returns>True if the input value is an integer power of two; otherwise false.</returns>
+        public static bool IsPowerOfTwo(long x)
+        {
+            // From: https://stackoverflow.com/questions/600293/how-to-check-if-a-number-is-a-power-of-2
+            return (x != 0) && (x & (x - 1)) == 0;
+        }
+
+        /// <summary>
+        /// Evaluate the binary logarithm of a non-zero Int32.
+        /// </summary>
+        /// <remarks>Two-step method using a De Bruijn-like sequence table lookup.</remarks>
+        public static int Log2(int x)
+        {
+            // Method from: https://stackoverflow.com/a/11398748/15703
+
+            x |= x >> 1;
+            x |= x >> 2;
+            x |= x >> 4;
+            x |= x >> 8;
+            x |= x >> 16;
+
+            return __log2_32[(uint)(x * 0x07C4ACDDU) >> 27];
+        }
+
+        /// <summary>
+        /// Evaluate the binary logarithm of a non-zero Int64.
+        /// </summary>
+        /// <remarks>Two-step method using a De Bruijn-like sequence table lookup.</remarks>
+        public static int Log2(long x)
+        {
+            // Method from: https://stackoverflow.com/a/11398748/15703
+
+            x |= x >> 1;
+            x |= x >> 2;
+            x |= x >> 4;
+            x |= x >> 8;
+            x |= x >> 16;
+            x |= x >> 32;
+
+            return __log2_64[((ulong)((x - (x >> 1)) * 0x07EDD5E59A4E28C2)) >> 58];
+        }
+
+
+        static readonly int[] __log2_32 = new int[32]
+        {
+            0,   9,  1, 10, 13, 21, 2, 29,
+            11, 14, 16, 18, 22, 25, 3, 30,
+            8,  12, 20, 28, 15, 17, 24, 7,
+            19, 27, 23,  6, 26,  5,  4, 31
+        };
+
+        static readonly int[] __log2_64 = new int[64]
+        {
+            63,  0, 58,  1, 59, 47, 53,  2,
+            60, 39, 48, 27, 54, 33, 42,  3,
+            61, 51, 37, 40, 49, 18, 28, 20,
+            55, 30, 34, 11, 43, 14, 22,  4,
+            62, 57, 46, 52, 38, 26, 32, 41,
+            50, 36, 17, 19, 29, 10, 13, 21,
+            56, 45, 25, 31, 35, 16,  9, 12,
+            44, 24, 15,  8, 23,  7,  6,  5
+        };
     }
 }
