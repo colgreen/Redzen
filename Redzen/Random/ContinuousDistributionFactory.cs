4fcaf2e5b8c0a505b29597cf3e73954d4c1798fd
Colin Green <colin.green1@gmail.com>
2018-05-10 11:35:29 +0100

    ContinuousDistributionFactory extended to match constructor signatures for uniform and gaussian distribution classes.

--- Redzen/Random/ContinuousDistributionFactory.cs
+++ Redzen/Random/ContinuousDistributionFactory.cs
@@ -7,24 +7,18 @@ namespace Redzen.Random
     /// </summary>
     public static class ContinuousDistributionFactory
     {
-        #region Public Static Methods
+        #region Public Static Methods [CreateUniformDistribution]
 
         /// <summary>
         /// Create a uniform distribution.
         /// </summary>
         /// <typeparam name="T">Data type of the individual samples.</typeparam>
         /// <returns>A new instance of <see cref="IContinuousDistribution{T}"/>.</returns>
-        public static IUniformDistribution<T> CreateUniformDistribution<T>() where T : struct
+        public static IUniformDistribution<T> CreateUniformDistribution<T>() 
+            where T : struct
         {
-            if(typeof(T) == typeof(double)) {
-                return (IUniformDistribution<T>)new Double.UniformDistribution();
-            }
-            else if(typeof(T) == typeof(float)) {
-                return (IUniformDistribution<T>)new Float.UniformDistribution();
-            }
-            else {
-                throw new ArgumentException("Unsupported type argument");
-            }
+            IRandomSource rng = RandomDefaults.CreateRandomSource();
+            return CreateUniformDistribution<T>(1.0, false, rng);
         }
 
         /// <summary>
@@ -32,17 +26,81 @@ namespace Redzen.Random
         /// </summary>
         /// <typeparam name="T">Data type of the individual samples.</typeparam>
         /// <returns>A new instance of <see cref="IContinuousDistribution{T}"/>.</returns>
-        public static IUniformDistribution<T> CreateUniformDistribution<T>(double scale, bool signed) where T : struct
+        public static IUniformDistribution<T> CreateUniformDistribution<T>(ulong seed) 
+            where T : struct
+        {
+            IRandomSource rng = RandomDefaults.CreateRandomSource(seed);
+            return CreateUniformDistribution<T>(1.0, false, rng);
+        }
+
+        /// <summary>
+        /// Create a uniform distribution.
+        /// </summary>
+        /// <typeparam name="T">Data type of the individual samples.</typeparam>
+        /// <returns>A new instance of <see cref="IContinuousDistribution{T}"/>.</returns>
+        public static IUniformDistribution<T> CreateUniformDistribution<T>(IRandomSource rng)
+            where T : struct
+        {
+            return CreateUniformDistribution<T>(1.0, false, rng);
+        }
+
+        /// <summary>
+        /// Create a uniform distribution.
+        /// </summary>
+        /// <typeparam name="T">Data type of the individual samples.</typeparam>
+        /// <returns>A new instance of <see cref="IContinuousDistribution{T}"/>.</returns>
+        public static IUniformDistribution<T> CreateUniformDistribution<T>(double scale, bool signed) 
+            where T : struct
+        {
+            IRandomSource rng = RandomDefaults.CreateRandomSource();
+            return CreateUniformDistribution<T>(scale, signed, rng);
+        }
+
+        /// <summary>
+        /// Create a uniform distribution.
+        /// </summary>
+        /// <typeparam name="T">Data type of the individual samples.</typeparam>
+        /// <returns>A new instance of <see cref="IContinuousDistribution{T}"/>.</returns>
+        public static IUniformDistribution<T> CreateUniformDistribution<T>(double scale, bool signed, ulong seed) 
+            where T : struct
+        {
+            IRandomSource rng = RandomDefaults.CreateRandomSource(seed);
+            return CreateUniformDistribution<T>(scale, signed, rng);
+        }
+
+        /// <summary>
+        /// Create a uniform distribution.
+        /// </summary>
+        /// <typeparam name="T">Data type of the individual samples.</typeparam>
+        /// <returns>A new instance of <see cref="IContinuousDistribution{T}"/>.</returns>
+        public static IUniformDistribution<T> CreateUniformDistribution<T>(double scale, bool signed, IRandomSource rng) 
+            where T : struct
         {
             if(typeof(T) == typeof(double)) {
-                return (IUniformDistribution<T>)new Double.UniformDistribution(scale, signed);
+                return (IUniformDistribution<T>)new Double.UniformDistribution(scale, signed, rng);
             }
             else if(typeof(T) == typeof(float)) {
-                return (IUniformDistribution<T>)new Float.UniformDistribution((float)scale, signed);
+                return (IUniformDistribution<T>)new Float.UniformDistribution((float)scale, signed, rng);
             }
             else {
                 throw new ArgumentException("Unsupported type argument");
             }
+        }
+
+        #endregion
+
+        #region Public Static Methods [CreateGaussianDistribution]
+
+        /// <summary>
+        /// Create a Gaussian distribution.
+        /// </summary>
+        /// <typeparam name="T">Data type of the individual samples.</typeparam>
+        /// <returns>A new instance of <see cref="IGaussianDistribution{T}"/>.</returns>
+        public static IGaussianDistribution<T> CreateGaussianDistribution<T>() 
+            where T : struct
+        {
+            IRandomSource rng = RandomDefaults.CreateRandomSource();
+            return CreateGaussianDistribution<T>(0.0, 1.0, rng);
         }
 
         /// <summary>
@@ -50,18 +108,11 @@ namespace Redzen.Random
         /// </summary>
         /// <typeparam name="T">Data type of the individual samples.</typeparam>
         /// <returns>A new instance of <see cref="IGaussianDistribution{T}"/>.</returns>
-        public static IGaussianDistribution<T> CreateGaussianDistribution<T>() where T : struct
+        public static IGaussianDistribution<T> CreateGaussianDistribution<T>(ulong seed) 
+            where T : struct
         {
-            if(typeof(T) == typeof(double)) {
-                return (IGaussianDistribution<T>)new Double.ZigguratGaussianDistribution();
-            }
-            else if(typeof(T) == typeof(float)) 
-            {
-                return (IGaussianDistribution<T>)new Float.ZigguratGaussianDistribution();
-            }
-            else {
-                throw new ArgumentException("Unsupported type argument");
-            }
+            IRandomSource rng = RandomDefaults.CreateRandomSource(seed);
+            return CreateGaussianDistribution<T>(0.0, 1.0, rng);
         }
 
         /// <summary>
@@ -69,14 +120,50 @@ namespace Redzen.Random
         /// </summary>
         /// <typeparam name="T">Data type of the individual samples.</typeparam>
         /// <returns>A new instance of <see cref="IGaussianDistribution{T}"/>.</returns>
-        public static IGaussianDistribution<T> CreateGaussianDistribution<T>(double mean, double stdDev) where T : struct
+        public static IGaussianDistribution<T> CreateGaussianDistribution<T>(IRandomSource rng) 
+            where T : struct
+        {
+            return CreateGaussianDistribution<T>(0.0, 1.0, rng);
+        }
+
+        /// <summary>
+        /// Create a Gaussian distribution.
+        /// </summary>
+        /// <typeparam name="T">Data type of the individual samples.</typeparam>
+        /// <returns>A new instance of <see cref="IGaussianDistribution{T}"/>.</returns>
+        public static IGaussianDistribution<T> CreateGaussianDistribution<T>(double mean, double stdDev) 
+            where T : struct
+        {
+            IRandomSource rng = RandomDefaults.CreateRandomSource();
+            return CreateGaussianDistribution<T>(mean, stdDev, rng);
+        }
+
+        /// <summary>
+        /// Create a Gaussian distribution.
+        /// </summary>
+        /// <typeparam name="T">Data type of the individual samples.</typeparam>
+        /// <returns>A new instance of <see cref="IGaussianDistribution{T}"/>.</returns>
+        public static IGaussianDistribution<T> CreateGaussianDistribution<T>(double mean, double stdDev, ulong seed) 
+            where T : struct
+        {
+            IRandomSource rng = RandomDefaults.CreateRandomSource(seed);
+            return CreateGaussianDistribution<T>(mean, stdDev, rng);
+        }
+
+        /// <summary>
+        /// Create a Gaussian distribution.
+        /// </summary>
+        /// <typeparam name="T">Data type of the individual samples.</typeparam>
+        /// <returns>A new instance of <see cref="IGaussianDistribution{T}"/>.</returns>
+        public static IGaussianDistribution<T> CreateGaussianDistribution<T>(double mean, double stdDev, IRandomSource rng) 
+            where T : struct
         {
             if(typeof(T) == typeof(double)) {
-                return (IGaussianDistribution<T>)new Double.ZigguratGaussianDistribution(mean, stdDev);
+                return (IGaussianDistribution<T>)new Double.ZigguratGaussianDistribution(mean, stdDev, rng);
             }
             else if(typeof(T) == typeof(float)) 
             {
-                return (IGaussianDistribution<T>)new Float.ZigguratGaussianDistribution(mean, stdDev);
+                return (IGaussianDistribution<T>)new Float.ZigguratGaussianDistribution(mean, stdDev, rng);
             }
             else {
                 throw new ArgumentException("Unsupported type argument");
