f3a8cb3257d0577ea39000860dfa3ed41645ee92
Colin Green <colin.green1@gmail.com>
2019-03-17 11:53:04 +0000

    Added MathArrayUtils and associated unit tests.

---
+++ Redzen.UnitTests/MathArrayUtilsTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Redzen.Numerics.Distributions.Double;
+
+namespace Redzen.UnitTests
+{
+    [TestClass]
+    public class MathArrayUtilsTests
+    {
+        #region Test Methods
+
+        [TestMethod]
+        [TestCategory("MathArrayUtils")]
+        public void SumSquaredDelta()
+        {
+            var sampler = new UniformDistributionSampler(100.0, true, 0);
+
+            // Test multiple times, with a range of array lengths;
+            // The vectorised code has edge cases related to array length, so this is a sensible test to do.
+            for(int len = 1; len < 20; len++) {
+                SumSquaredDelta(sampler, len);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("MathArrayUtils")]
+        public void MeanSquaredDelta()
+        {
+            var sampler = new UniformDistributionSampler(100.0, true, 0);
+
+            // Test multiple times, with a range of array lengths;
+            // The vectorised code has edge cases related to array length, so this is a sensible test to do.
+            for(int len = 1; len < 20; len++) {
+                MeanSquaredDelta(sampler, len);
+            }
+        }
+
+        #endregion
+
+        #region Private Static Methods
+
+        private static void SumSquaredDelta(UniformDistributionSampler sampler, int len)
+        {
+            // Alloc arrays and fill with uniform random noise.
+            double[] a = new double[len];
+            double[] b = new double[len];
+            sampler.Sample(a);
+            sampler.Sample(b);
+
+            // Calc results and compare.
+            double expected = SumSquaredDelta(a, b);
+            double actual = MathArrayUtils.SumSquaredDelta(a, b);
+            Assert.AreEqual(expected, actual, 1e-10);
+        }
+
+        private static void MeanSquaredDelta(UniformDistributionSampler sampler, int len)
+        {
+            // Alloc arrays and fill with uniform random noise.
+            double[] a = new double[len];
+            double[] b = new double[len];
+            sampler.Sample(a);
+            sampler.Sample(b);
+
+            // Calc results and compare.
+            double expected = SumSquaredDelta(a, b) / a.Length;
+            double actual = MathArrayUtils.MeanSquaredDelta(a, b);
+            Assert.AreEqual(expected, actual, 1e-10);
+        }
+
+        private static double SumSquaredDelta(double[] a, double[] b)
+        {
+            double total = 0.0;
+
+            // Calc sum(squared error).
+            for(int i=0; i < a.Length; i++)
+            {
+                double err = a[i] - b[i];
+                total += err * err;
+            }
+
+            return total;
+        }
+
+        #endregion
+    }
+}
---
+++ Redzen/MathArrayUtils.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Numerics;
+
+namespace Redzen
+{
+    /// <summary>
+    /// Math utility methods for working with arrays.
+    /// </summary>
+    public static class MathArrayUtils
+    {
+        #region Public Static Methods
+
+        /// <summary>
+        /// Calculate the mean squared difference of the elements in arrays {a} and {b}.
+        /// </summary>
+        /// <param name="a">Array {a}.</param>
+        /// <param name="b">Array {b}.</param>
+        /// <returns>A double.</returns>
+        /// <remarks>Arrays {a} and {b} must be the same length.</remarks>
+        public static double MeanSquaredDelta(double[] a, double[] b)
+        {
+            return SumSquaredDelta(a, b) / a.Length;
+        }
+
+        /// <summary>
+        /// Calculate the sum of the squared difference of each elements in arrays {a} and {b}.
+        /// </summary>
+        /// <param name="a">Array {a}.</param>
+        /// <param name="b">Array {b}.</param>
+        /// <returns>A double.</returns>
+        /// <remarks>Arrays {a} and {b} must be the same length.</remarks>
+        public static double SumSquaredDelta(double[] a, double[] b)
+        {
+            if(a.Length != b.Length) throw new ArgumentException("Array lengths are not equal.");
+
+            double total = 0.0;
+
+            if(Vector.IsHardwareAccelerated)
+            {
+                int width = Vector<double>.Count;
+                var sumVec = new Vector<double>(0.0);
+
+                // Loop over vector sized segments, calc the squared error for each, and accumulate in sumVec.
+                int idx=0;
+                for(; idx <= a.Length - width; idx += width)
+                {
+                    var aVec = new Vector<double>(a, idx);
+                    var bVec = new Vector<double>(b, idx);
+
+                    var cVec = aVec - bVec;
+                    sumVec += cVec * cVec;
+                }
+
+                // Sum the elements of sumVec.
+                for(int j=0; j < width; j++){
+                    total += sumVec[j];
+                }
+
+                // Handle remaining elements (if any).
+                for(; idx < a.Length; idx++)
+                {
+                    double err = a[idx] - b[idx];
+                    total += err * err;
+                }
+            }
+            else
+            {
+                // Calc sum(squared error).
+                for(int i=0; i < a.Length; i++)
+                {
+                    double err = a[i] - b[i];
+                    total += err * err;
+                }
+            }
+            return total;
+        }
+
+        #endregion
+    }
+}
