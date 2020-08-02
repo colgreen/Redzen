ec951a85d0fb462e9eaec6a7a948e65476efece6
colgreen <colin.green1@gmail.com>
2020-08-02 14:27:45 +0100

    Added MathFSpanUtils.

---
+++ Redzen.UnitTests/MathFSpanUtilsTests.cs
@@ -0,0 +1,169 @@
+﻿using System.Diagnostics;
+using Redzen.Numerics.Distributions.Float;
+using Xunit;
+
+namespace Redzen.UnitTests
+{
+    public class MathFSpanUtilsTests
+    {
+        #region Test Methods
+
+        [Fact]
+        public void Clip()
+        {
+            var sampler = new UniformDistributionSampler(20f, true, 0);
+
+            // Test with a range of array lengths;
+            // the vectorised code has edge cases related to array length, so this is a sensible test to do.
+            for(int len = 1; len < 20; len++) {
+                Clip_Inner(sampler, len);
+            }
+        }
+
+        [Fact]
+        public void MeanSquaredDelta()
+        {
+            var sampler = new UniformDistributionSampler(10f, true, 0);
+
+            // Test with a range of array lengths;
+            // the vectorised code has edge cases related to array length, so this is a sensible test to do.
+            for(int len = 1; len < 20; len++) {
+                MeanSquaredDelta_Inner(sampler, len);
+            }
+        }
+
+        [Fact]
+        public void SumSquaredDelta()
+        {
+            var sampler = new UniformDistributionSampler(10f, true, 0);
+
+            // Test with a range of array lengths;
+            // the vectorised code has edge cases related to array length, so this is a sensible test to do.
+            for(int len = 1; len < 20; len++) {
+                SumSquaredDelta_Inner(sampler, len);
+            }
+        }
+
+        [Fact]
+        public void MinMax()
+        {
+            var sampler = new UniformDistributionSampler(100f, true, 0);
+
+            // Test with a range of array lengths;
+            // the vectorised code has edge cases related to array length, so this is a sensible test to do.
+            for(int len = 1; len < 20; len++) {
+                MinMax_Inner(sampler, len);
+            }
+        }
+
+        #endregion
+
+        #region Private Static Methods [Test Subroutines]
+
+        private static void Clip_Inner(UniformDistributionSampler sampler, int len)
+        {
+            // Alloc array and fill with uniform random noise.
+            float[] x = new float[len];
+            sampler.Sample(x);
+
+            // Clip the elements of the array with the safe routine.
+            float[] expected = (float[])x.Clone();
+            PointwiseClip(expected, -1.1f, 18.8f);
+
+            // Clip the elements of the array.
+            float[] actual = (float[])x.Clone();
+            MathFSpanUtils.Clip(actual, -1.1f, 18.8f);
+
+            // Compare expected with actual array.
+            Assert.True(ArrayUtils.Equals(expected, actual));
+        }
+
+        private static void SumSquaredDelta_Inner(UniformDistributionSampler sampler, int len)
+        {
+            // Alloc arrays and fill with uniform random noise.
+            float[] a = new float[len];
+            float[] b = new float[len];
+            sampler.Sample(a);
+            sampler.Sample(b);
+
+            // Calc results and compare.
+            float expected = PointwiseSumSquaredDelta(a, b);
+            float actual = MathFSpanUtils.SumSquaredDelta(a, b);
+            Assert.Equal(expected, actual, 10);
+        }
+
+        private static void MeanSquaredDelta_Inner(UniformDistributionSampler sampler, int len)
+        {
+            // Alloc arrays and fill with uniform random noise.
+            float[] a = new float[len];
+            float[] b = new float[len];
+            sampler.Sample(a);
+            sampler.Sample(b);
+
+            // Calc results and compare.
+            float expected = PointwiseSumSquaredDelta(a, b) / a.Length;
+            float actual = MathFSpanUtils.MeanSquaredDelta(a, b);
+            Assert.Equal(expected, actual, 10);
+        }
+
+        private static void MinMax_Inner(UniformDistributionSampler sampler, int len)
+        {
+            // Alloc arrays and fill with uniform random noise.
+            float[] a = new float[len];
+            sampler.Sample(a);
+
+            // Calc results and compare.
+            PointwiseMinMax(a, out float expectedMin, out float expectedMax);
+            MathFSpanUtils.MinMax(a, out float actualMin, out float actualMax);
+
+            Assert.Equal(expectedMin, actualMin, 10);
+            Assert.Equal(expectedMax, actualMax, 10);
+        }
+
+        #endregion
+
+        #region Private Static Methods [Scalar Math Routines]
+
+        private static void PointwiseClip(float[] x, float min, float max)
+        {
+            for(int i=0; i < x.Length; i++)
+            {
+                if(x[i] < min)
+                    x[i] = min;
+                else if(x[i] > max)
+                    x[i] = max;
+            }
+        }
+
+        private static float PointwiseSumSquaredDelta(float[] a, float[] b)
+        {
+            float total = 0f;
+
+            // Calc sum(squared error).
+            for(int i=0; i < a.Length; i++)
+            {
+                float err = a[i] - b[i];
+                total += err * err;
+            }
+
+            return total;
+        }
+
+        private static void PointwiseMinMax(float[] a, out float min, out float max)
+        {
+            min = max = a[0];
+            for(int i=1; i < a.Length; i++)
+            {
+                float val = a[i];
+                if(val < min) {
+                    min = val;
+                }
+                else if(val > max) {
+                    max = val;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Redzen/MathFSpanUtils.cs
@@ -0,0 +1,183 @@
+﻿/* ***************************************************************************
+ * This file is part of the Redzen code library.
+ * 
+ * Copyright 2015-2020 Colin Green (colin.green1@gmail.com)
+ *
+ * Redzen is free software; you can redistribute it and/or modify
+ * it under the terms of The MIT License (MIT).
+ *
+ * You should have received a copy of the MIT License
+ * along with Redzen; if not, see https://opensource.org/licenses/MIT.
+ */
+using System;
+using System.Numerics;
+
+namespace Redzen
+{
+    /// <summary>
+    /// Math utility methods for working with spans.
+    /// </summary>
+    public static class MathFSpanUtils
+    {
+        #region Public Static Methods
+
+        /// <summary>
+        /// Clip (limit) the values in an array.
+        /// For example, if an interval of [0, 1] is specified, values smaller than 0 become 0, and values larger than 1 become 1.
+        /// </summary>
+        /// <param name="x">Array containing the elements to clip.</param>
+        /// <param name="min">Minimum value.</param>
+        /// <param name="max">Maximum value.</param>
+        public static void Clip(Span<float> x, float min, float max)
+        {
+            int idx=0;
+
+            // Run the vectorised code only if the hardware acceleration is available, and there are 
+            // enough array elements to utilise it.
+            if(Vector.IsHardwareAccelerated && (x.Length >= Vector<float>.Count))
+            {
+                int width = Vector<float>.Count;
+                var minVec = new Vector<float>(min);
+                var maxVec = new Vector<float>(max);
+
+                // Loop over vector sized segments.
+                for(; idx <= x.Length - width; idx += width)
+                {
+                    Span<float> slice = x.Slice(idx, width);
+                    var xv = new Vector<float>(slice);
+                    xv = Vector.Max(minVec, xv);
+                    xv = Vector.Min(maxVec, xv);
+                    xv.CopyTo(slice);
+                }
+            }
+
+            // Note. If the above vector logic block was executed then this handles remaining elements,
+            // otherwise it handles all elements.
+            for(; idx < x.Length; idx++)
+            {
+                if(x[idx] < min)
+                    x[idx] = min;
+                else if(x[idx] > max)
+                    x[idx] = max;
+            }
+        }
+
+        /// <summary>
+        /// Calculate the mean squared difference of the elements in arrays {a} and {b}.
+        /// </summary>
+        /// <param name="a">Array {a}.</param>
+        /// <param name="b">Array {b}.</param>
+        /// <returns>A float.</returns>
+        /// <remarks>Arrays {a} and {b} must be the same length.</remarks>
+        public static float MeanSquaredDelta(Span<float> a, Span<float> b)
+        {
+            return SumSquaredDelta(a, b) / a.Length;
+        }
+
+        /// <summary>
+        /// Calculate the sum of the squared difference of each elements in arrays {a} and {b}.
+        /// </summary>
+        /// <param name="a">Array {a}.</param>
+        /// <param name="b">Array {b}.</param>
+        /// <returns>A float.</returns>
+        /// <remarks>Arrays {a} and {b} must be the same length.</remarks>
+        public static float SumSquaredDelta(Span<float> a, Span<float> b)
+        {
+            if(a.Length != b.Length) throw new ArgumentException("Array lengths are not equal.");
+
+            float total = 0f;
+            int idx=0;
+
+            // Run the vectorised code only if the hardware acceleration is available, and there are 
+            // enough array elements to utilise it.
+            if(Vector.IsHardwareAccelerated && (a.Length >= Vector<float>.Count))
+            {
+                int width = Vector<float>.Count;
+                var sumVec = new Vector<float>(0f);
+
+                // Loop over vector sized segments, calc the squared error for each, and accumulate in sumVec.
+                for(; idx <= a.Length - width; idx += width)
+                {
+                    var av = new Vector<float>(a.Slice(idx, width));
+                    var bv = new Vector<float>(b.Slice(idx, width));
+
+                    var cv = av - bv;
+                    sumVec += cv * cv;
+                }
+
+                // Sum the elements of sumVec.
+                for(int j=0; j < width; j++) {
+                    total += sumVec[j];
+                }
+            }
+
+            // Calc sum(squared error).
+            // Note. If the above vector logic block was executed then this handles remaining elements,
+            // otherwise it handles all elements.
+            for(; idx < a.Length; idx++)
+            {
+                float err = a[idx] - b[idx];
+                total += err * err;
+            }
+            
+            return total;
+        }
+
+        /// <summary>
+        /// Calculate the minimum and maximum values in the provided array.
+        /// </summary>
+        /// <param name="a">The array.</param>
+        /// <param name="min">Returns the minimum value in the array.</param>
+        /// <param name="max">Returns the maximum value in the array.</param>
+        public static void MinMax(Span<float> a, out float min, out float max)
+        {
+            int idx=0;
+
+            // Run the vectorised code only if the hardware acceleration is available, and there are 
+            // enough array elements to utilise it.
+            if(Vector.IsHardwareAccelerated && (a.Length >= Vector<float>.Count))
+            {
+                int width = Vector<float>.Count;
+                var minVec = new Vector<float>(a[0]);
+                var maxVec = new Vector<float>(a[0]);
+
+                // Loop over vector sized segments.
+                for(; idx <= a.Length - width; idx += width)
+                {
+                    var xv = new Vector<float>(a.Slice(idx, width));
+                    minVec = Vector.Min(minVec, xv);
+                    maxVec = Vector.Max(maxVec, xv);
+                }
+
+                // Calc min(minVec) and max(maxVec).
+                min = max = a[0];
+                for(int j=0; j < width; j++)
+                {
+                    if(minVec[j] < min) min = minVec[j];
+                    if(maxVec[j] > max) max = maxVec[j];
+                }
+            }
+            else
+            {
+                min = max = a[0];
+                idx = 1;
+            }
+
+            // Calc min/max.
+            // Note. If the above vector logic block was executed then this handles remaining elements,
+            // otherwise it handles all elements.
+            for(; idx < a.Length; idx++)
+            {
+                float val = a[idx];
+                if(val < min) {
+                    min = val;
+                }
+                else if(val > max) {
+                    max = val;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
