b392ab228aaf0ecc1e44bbecc99610fb7ef4a3b9
colgreen <colin.green1@gmail.com>
2020-11-08 22:38:40 +0000

    EnumerableUtils.RangeRandomOrder() now gets a temp array from ArrayPool.

--- Redzen/Linq/EnumerableUtils.cs
+++ Redzen/Linq/EnumerableUtils.cs
@@ -10,6 +10,7 @@
  * along with Redzen; if not, see https://opensource.org/licenses/MIT.
  */
 using System;
+using System.Buffers;
 using System.Collections.Generic;
 using Redzen.Random;
 
@@ -31,33 +32,41 @@ namespace Redzen.Linq
 		        throw new ArgumentOutOfRangeException("count");
 	        }
 
-            // Create an array of all indexes to be yielded.
-            int[] arr = new int[count];
-            for(int i=0; i < count; i++) {
-                arr[i] = start + i;
+            // Initialise an array of all indexes to be yielded.
+            int[] arr = ArrayPool<int>.Shared.Rent(count);
+
+            try
+            {
+                for(int i=0; i < count; i++) {
+                    arr[i] = start + i;
+                }
+
+                // Yield all values in turn, applying a Fisher–Yates shuffle as we go in order to randomize the yield order.
+                // See https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+	            for (int i=count-1; i > 0; i--)
+	            {
+                    // Select at random from the remaining available slots.
+                    int selectIdx = rng.Next(i + 1);
+
+                    // Store the yield value.
+                    int tmp = arr[selectIdx];
+
+                    // Replace the selected slot value with a value that has not yet been selected.
+                    // This is half of the Fisher–Yates swap, but since we don't need the final 
+                    // shuffled array we can omit moving the yielded value into its new slot.
+                    arr[selectIdx] = arr[i];
+
+                    // Yield the value from the randomly selected slot.
+		            yield return tmp;
+	            }
+
+                // Yield final value.
+                yield return arr[0];
             }
-
-            // Yield all values in turn, applying a Fisher–Yates shuffle as we go in order to randomize the yield order.
-            // See https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-	        for (int i=count-1; i > 0; i--)
-	        {
-                // Select at random from the remaining available slots.
-                int selectIdx = rng.Next(i + 1);
-
-                // Store the yield value.
-                int tmp = arr[selectIdx];
-
-                // Replace the selected slot value with a value that has not yet been selected.
-                // This is half of the Fisher–Yates swap, but since we don't need the final 
-                // shuffled array we can omit moving the yielded value into its new slot.
-                arr[selectIdx] = arr[i];
-
-                // Yield the value from the randomly selected slot.
-		        yield return tmp;
-	        }
-
-            // Yield final value.
-            yield return arr[0];	        
+            finally
+            {
+                ArrayPool<int>.Shared.Return(arr);
+            }
         }
     }
 }
