4a7be49e8e865b080bb8f8e630ebaf27e544efc5
Colin Green <colin.green1@gmail.com>
2021-12-31 15:06:44 +0000

    Added DoubleUtils, SingleUtils.

---
+++ Redzen.Tests/DoubleUtilsTests.cs
@@ -0,0 +1,84 @@
+﻿using Xunit;
+using FluentAssertions;
+using System;
+
+namespace Redzen.Tests
+{
+    public class DoubleUtilsTests
+    {
+        [Theory]
+        [InlineData(-1.0f, false)]
+        [InlineData(-0.000001f, false)]
+        [InlineData(-double.Epsilon, false)]
+        [InlineData(-1.23e16f, false)]
+        [InlineData(double.NaN, false)]
+        [InlineData(double.PositiveInfinity, false)]
+        [InlineData(double.NegativeInfinity, false)]
+        [InlineData(0f, true)]
+        [InlineData(0.000001f, true)]
+        [InlineData(double.Epsilon, true)]
+        [InlineData(1.23e16f, true)]
+        public void IsNonNegativeReal(double f, bool expected)
+        {
+            DoubleUtils.IsNonNegativeReal(f).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(-1.0f, false)]
+        [InlineData(-0.000001f, false)]
+        [InlineData(-double.Epsilon, false)]
+        [InlineData(-1.23e16f, false)]
+        [InlineData(double.NaN, false)]
+        [InlineData(double.PositiveInfinity, false)]
+        [InlineData(double.NegativeInfinity, false)]
+        [InlineData(0f, false)]
+        [InlineData(0.000001f, true)]
+        [InlineData(double.Epsilon, true)]
+        [InlineData(1.23e16f, true)]
+        public void IsPositiveReal(double f, bool expected)
+        {
+            DoubleUtils.IsPositiveReal(f).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(-1.0f, false)]
+        [InlineData(-0.000001f, false)]
+        [InlineData(-double.Epsilon, false)]
+        [InlineData(-1.23e16f, false)]
+        [InlineData(double.NaN, false)]
+        [InlineData(double.PositiveInfinity, false)]
+        [InlineData(double.NegativeInfinity, false)]
+        [InlineData(0f, true)]
+        [InlineData(0.000001f, true)]
+        [InlineData(double.Epsilon, true)]
+        [InlineData(1.23e16f, true)]
+        public void AllNonNegativeReal(double f, bool expected)
+        {
+            var vals = new double[100];
+
+            vals[63] = f;
+            DoubleUtils.AllNonNegativeReal(vals).Should().Be(expected);            
+        }
+
+        [Theory]
+        [InlineData(-1.0f, false)]
+        [InlineData(-0.000001f, false)]
+        [InlineData(-double.Epsilon, false)]
+        [InlineData(-1.23e16f, false)]
+        [InlineData(double.NaN, false)]
+        [InlineData(double.PositiveInfinity, false)]
+        [InlineData(double.NegativeInfinity, false)]
+        [InlineData(0f, false)]
+        [InlineData(0.000001f, true)]
+        [InlineData(double.Epsilon, true)]
+        [InlineData(1.23e16f, true)]
+        public void AllPositiveReal(double f, bool expected)
+        {
+            var vals = new double[100];
+            Array.Fill(vals, 1);
+
+            vals[63] = f;
+            DoubleUtils.AllPositiveReal(vals).Should().Be(expected);
+        }
+    }
+}
---
+++ Redzen.Tests/SingleUtilsTests.cs
@@ -0,0 +1,84 @@
+﻿using Xunit;
+using FluentAssertions;
+using System;
+
+namespace Redzen.Tests
+{
+    public class SingleUtilsTests
+    {
+        [Theory]
+        [InlineData(-1.0f, false)]
+        [InlineData(-0.000001f, false)]
+        [InlineData(-float.Epsilon, false)]
+        [InlineData(-1.23e16f, false)]
+        [InlineData(float.NaN, false)]
+        [InlineData(float.PositiveInfinity, false)]
+        [InlineData(float.NegativeInfinity, false)]
+        [InlineData(0f, true)]
+        [InlineData(0.000001f, true)]
+        [InlineData(float.Epsilon, true)]
+        [InlineData(1.23e16f, true)]
+        public void IsNonNegativeReal(float f, bool expected)
+        {
+            SingleUtils.IsNonNegativeReal(f).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(-1.0f, false)]
+        [InlineData(-0.000001f, false)]
+        [InlineData(-float.Epsilon, false)]
+        [InlineData(-1.23e16f, false)]
+        [InlineData(float.NaN, false)]
+        [InlineData(float.PositiveInfinity, false)]
+        [InlineData(float.NegativeInfinity, false)]
+        [InlineData(0f, false)]
+        [InlineData(0.000001f, true)]
+        [InlineData(float.Epsilon, true)]
+        [InlineData(1.23e16f, true)]
+        public void IsPositiveReal(float f, bool expected)
+        {
+            SingleUtils.IsPositiveReal(f).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(-1.0f, false)]
+        [InlineData(-0.000001f, false)]
+        [InlineData(-float.Epsilon, false)]
+        [InlineData(-1.23e16f, false)]
+        [InlineData(float.NaN, false)]
+        [InlineData(float.PositiveInfinity, false)]
+        [InlineData(float.NegativeInfinity, false)]
+        [InlineData(0f, true)]
+        [InlineData(0.000001f, true)]
+        [InlineData(float.Epsilon, true)]
+        [InlineData(1.23e16f, true)]
+        public void AllNonNegativeReal(float f, bool expected)
+        {
+            var vals = new float[100];
+
+            vals[63] = f;
+            SingleUtils.AllNonNegativeReal(vals).Should().Be(expected);            
+        }
+
+        [Theory]
+        [InlineData(-1.0f, false)]
+        [InlineData(-0.000001f, false)]
+        [InlineData(-float.Epsilon, false)]
+        [InlineData(-1.23e16f, false)]
+        [InlineData(float.NaN, false)]
+        [InlineData(float.PositiveInfinity, false)]
+        [InlineData(float.NegativeInfinity, false)]
+        [InlineData(0f, false)]
+        [InlineData(0.000001f, true)]
+        [InlineData(float.Epsilon, true)]
+        [InlineData(1.23e16f, true)]
+        public void AllPositiveReal(float f, bool expected)
+        {
+            var vals = new float[100];
+            Array.Fill(vals, 1);
+
+            vals[63] = f;
+            SingleUtils.AllPositiveReal(vals).Should().Be(expected);
+        }
+    }
+}
