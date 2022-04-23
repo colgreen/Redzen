9bc1d675a2af6caf4e7696eafef43c807719971c
Colin Green <colin.green1@gmail.com>
2022-04-23 13:53:55 +0100

    Replaced per-file licensing header with a shorter two-line version. This reduces visual clutter and eliminates the need to update the copyright year on all files each year!

--- Redzen/Numerics/NumericsUtils.cs
+++ Redzen/Numerics/NumericsUtils.cs
@@ -1,14 +1,5 @@
-﻿/* ***************************************************************************
- * This file is part of the Redzen code library.
- *
- * Copyright 2015-2022 Colin Green (colin.green1@gmail.com)
- *
- * Redzen is free software; you can redistribute it and/or modify
- * it under the terms of The MIT License (MIT).
- *
- * You should have received a copy of the MIT License
- * along with Redzen; if not, see https://opensource.org/licenses/MIT.
- */
+﻿// This file is part of the Redzen code library; Copyright Colin D. Green.
+// See LICENSE.txt for details.
 using Redzen.Random;
 
 namespace Redzen.Numerics;
--- Redzen/Random/WyRandom.cs
+++ Redzen/Random/WyRandom.cs
@@ -1,14 +1,6 @@
-﻿/* ***************************************************************************
- * This file is part of the Redzen code library.
- *
- * Copyright 2015-2022 Colin Green (colin.green1@gmail.com)
- *
- * Redzen is free software; you can redistribute it and/or modify
- * it under the terms of The MIT License (MIT).
- *
- * You should have received a copy of the MIT License
- * along with Redzen; if not, see https://opensource.org/licenses/MIT.
- */
+﻿// This file is part of the Redzen code library; Copyright Colin D. Green.
+// See LICENSE.txt for details.
+// The underlying algorithm and portions of this source file are from https://github.com/wangyi-fudan/wyhash.
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics.X86;
