f61f1015cb959f32115a5e0148a382879d7ba1b5
colgreen <colin.green1@gmail.com>
2022-11-27 21:57:23 +0000

    Added RentedArraySpans.

---
+++ Redzen.Tests/Buffers/RentedArraySpansTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Redzen.Buffers;
+
+public class RentedArraySpansTests
+{
+    [Fact]
+    public void SpanLengthsShouldMatchSpecifiedLengths()
+    {
+        int[] lengths = new int[] { 0, 1, 10, 23};
+
+        var rented = new RentedArraySpans<int>(lengths);
+
+        rented.Should().NotBeNull();
+
+        for(int i=0; i< lengths.Length; i++) 
+        { 
+            rented.GetSpan(i).Length.Should().Be(lengths[i]);
+        }
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(4)]
+    public void RequestedInvalidSegmentIndexShouldThrow(int segmentIdx)
+    {
+        int[] lengths = new int[] { 0, 1, 10, 23 };
+
+        var rented = new RentedArraySpans<int>(lengths);
+
+        Action getSpanAction = () => rented.GetSpan(segmentIdx);
+
+        getSpanAction.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void RentingNegativeLengthShouldThrow()
+    {
+        int[] lengths = new int[] { 0, 10, -1 };
+
+        Func<RentedArraySpans<int>> cstr = () => new RentedArraySpans<int>(lengths);
+
+        cstr.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void SegmentSpansShouldNotOverlap()
+    {
+        int[] lengths = new int[] { 2, 8, 10, 23};
+
+        var rented = new RentedArraySpans<int>(lengths);
+
+        var span0 = rented.GetSpan(0);
+        var span1 = rented.GetSpan(1);
+        var span2 = rented.GetSpan(2);
+        var span3 = rented.GetSpan(3);
+
+        MemoryExtensions.Overlaps(span0, span1).Should().BeFalse();
+        MemoryExtensions.Overlaps(span0, span2).Should().BeFalse();
+        MemoryExtensions.Overlaps(span0, span3).Should().BeFalse();
+
+        MemoryExtensions.Overlaps(span1, span2).Should().BeFalse();
+        MemoryExtensions.Overlaps(span1, span3).Should().BeFalse();
+
+        MemoryExtensions.Overlaps(span2, span3).Should().BeFalse();
+    }
+}
---
+++ Redzen/Buffers/RentedArraySpans.cs
@@ -0,0 +1,68 @@
+﻿// This file is part of the Redzen code library; Copyright Colin D. Green.
+// See LICENSE.txt for details.
+using System.Buffers;
+
+namespace Redzen.Buffers;
+
+/// <summary>
+/// Wraps an array rented from an <see cref="ArrayPool{T}"/> and exposes one or more segments within the array as
+/// spans.
+/// </summary>
+/// <typeparam name="T">Array/Span element type.</typeparam>
+/// <remarks>
+/// This type is <see cref="IDisposable"/>; calling <see cref="IDisposable.Dispose"/> will result in the wrapped
+/// rented array being returned to the <see cref="ArrayPool{T}"/>.
+/// </remarks>
+public readonly struct RentedArraySpans<T> : IDisposable
+{
+    private readonly Memory<T>[] _segments;
+    private readonly T[] _arr;
+
+    /// <summary>
+    /// Constructs a new <see cref="RentedArraySpans{T}"/> that provides multiple spans of the specified lengths.
+    /// </summary>
+    /// <param name="lengths">Specifies the lengths of the required spans. The number of lengths determine the number
+    /// of require spans.</param>
+    public RentedArraySpans(
+        Span<int> lengths)
+    {
+        int sumLengths = 0;
+        for(int i=0; i<lengths.Length; i++)
+        {
+            if(lengths[i] < 0)
+                throw new ArgumentException("One or more of the provided length values is negative.", nameof(lengths));
+
+            sumLengths += lengths[i];
+        }
+
+        // Rent an array that will be used as the storage for all of the required spans.
+        _arr = ArrayPool<T>.Shared.Rent(sumLengths);
+
+        // Create the Memory<T> segments.
+        _segments = new Memory<T>[lengths.Length];
+
+        for(int i = 0, startIdx = 0; i < lengths.Length; i++)
+        {
+            _segments[i] = new Memory<T>(_arr, startIdx, lengths[i]);
+            startIdx += lengths[i];
+        }
+    }
+
+    /// <summary>
+    /// Get a span for the specified segment.
+    /// </summary>
+    /// <param name="segmentIdx">The segment index to get the span for.</param>
+    /// <returns>A span over the specified segment's elements.</returns>
+    public Span<T> GetSpan(int segmentIdx)
+    {
+        if (segmentIdx < 0 || segmentIdx >= _segments.Length) throw new ArgumentOutOfRangeException(nameof(segmentIdx));
+
+        return _segments[segmentIdx].Span;
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        ArrayPool<T>.Shared.Return(_arr);
+    }
+}
