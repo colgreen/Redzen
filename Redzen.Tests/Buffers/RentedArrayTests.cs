b31ff4eda74d61a1f945881a5c553b375ff74331
colgreen <colin.green1@gmail.com>
2022-12-28 12:46:03 +0000

    Resurrected RentedArray; on reflection, I think this may be useful in some scenarios.

---
+++ Redzen.Tests/Buffers/RentedArrayTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Redzen.Buffers;
+
+public class RentedArraySpanTests
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(10)]
+    [InlineData(23)]
+    [InlineData(100)]
+    [InlineData(101)]
+    public void RentedArrayShouldHaveCorrectLength(int length)
+    {
+        using var rented = new RentedArray<int>(length);
+
+        rented.Should().NotBeNull();
+        var span = rented.AsSpan();
+        span.Length.Should().Be(length);
+    }
+
+    [Fact]
+    public void RentingNegativeLengthShouldThrow()
+    {
+        Func<RentedArray<int>> cstr = () => new RentedArray<int>(-1);
+
+        cstr.Should().Throw<ArgumentOutOfRangeException>();
+    }
+}
---
+++ Redzen/Buffers/RentedArray.cs
@@ -0,0 +1,46 @@
+﻿// This file is part of the Redzen code library; Copyright Colin D. Green.
+// See LICENSE.txt for details.
+using System.Buffers;
+
+namespace Redzen.Buffers;
+
+/// <summary>
+/// Wraps an array rented from an <see cref="ArrayPool{T}"/>, and exposes a specified number of elements from it as
+/// a <see cref="Span{T}"/>.
+/// </summary>
+/// <typeparam name="T">Array/Span element type.</typeparam>
+/// <remarks>
+/// This type is <see cref="IDisposable"/>; calling <see cref="IDisposable.Dispose"/> will result in the wrapped
+/// rented array being returned to <see cref="ArrayPool{T}.Shared"/>.
+/// </remarks>
+public readonly struct RentedArray<T> : IDisposable
+{
+    private readonly int _length;
+    private readonly T[] _arr;
+
+    /// <summary>
+    /// Constructs a new <see cref="RentedArray{T}"/> that provides a <see cref="Span{T}"/> of the specified length.
+    /// </summary>
+    /// <param name="length">The length of the required span.</param>
+    public RentedArray(int length)
+    {
+        if(length < 0) throw new ArgumentOutOfRangeException(nameof(length));
+        _length = length;
+        _arr = ArrayPool<T>.Shared.Rent(length);
+    }
+
+    /// <summary>
+    /// Creates a new span of the required length over wrapped rented array.
+    /// </summary>
+    /// <returns>A new <see cref="Span{T}"/>.</returns>
+    public Span<T> AsSpan()
+    {
+        return _arr.AsSpan(0, _length);
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        ArrayPool<T>.Shared.Return(_arr);
+    }
+}
